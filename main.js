a9129b26bf13cbd9d80efe84130882fff6158ea0
geomell <georgia.melissari@gmail.com>
2022-05-19 21:18:09 +0300

    filters&responsive

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 let menuIcon = document.querySelector(".menu-icon");
 let sideBar = document.querySelector(".sidebar");
 let container=document.querySelector(".container");
+let filters=document.querySelector(".filters");
 
 menuIcon.onclick = function(){
     sideBar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
-    
+    filters.classList.toggle("large-filters");
 }
 
-let deleteSearch=document.querySelector(".delete");
+let deleteSearch=document.querySelector(".nav__delete");
 
 deleteSearch.addEventListener('click', function (){
     let inputSearch=document.getElementById('input');
